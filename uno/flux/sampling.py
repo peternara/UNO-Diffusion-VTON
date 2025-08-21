0ccc01457e78cee26ae55d6b9c9d1c13c15f4fc5
peternara <peternara@users.noreply.github.com>
2025-08-21 18:07:58 +0900

    Update sampling.py

--- uno/flux/sampling.py
+++ uno/flux/sampling.py
@@ -133,7 +133,7 @@ def prepare_multi_ip(
         bs = len(prompt)
 
     # 1. 입력 이미지 Patchify 처리 & ID 생성    
-    img = rearrange(img, "b c (h ph) (w pw) -> b (h w) (c ph pw)", ph=2, pw=2) # 이미지를 2x2 patch 단위로 분해 → shape: (B, H//2 * W//2, C22)
+    img = rearrange(img, "b c (h ph) (w pw) -> b (h w) (c ph pw)", ph=2, pw=2) # 이미지를 2x2 patch 단위로 분해 → shape: (B, H//2 * W//2, C*2*2)
     if img.shape[0] == 1 and bs > 1: # 이미지 배치가 1이고 실제 배치가 여러개면
         # 만약 배치가 1이고, bs > 1이면 이미지를 bs개로 복제
         img = repeat(img, "1 ... -> bs ...", bs=bs)
