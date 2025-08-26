19196139e164744e7e4addbd8413e73edb5ad7e9
peternara <peternara@users.noreply.github.com>
2025-08-26 15:58:58 +0900

    Update sampling.py

--- uno/flux/sampling.py
+++ uno/flux/sampling.py
@@ -223,13 +223,13 @@ def prepare_multi_ip(
         vec = repeat(vec, "1 ... -> bs ...", bs=bs)
 
     return {
-        "img": img,                                                               # (B, 패치수, 패치채널)
-        "img_ids": img_ids.to(img.device),                                        # 이미지 patch별 positional id
-        "ref_img": tuple(ref_imgs_list),                                          # 참조 이미지 패치화 결과
-        "ref_img_ids": [ref_img_id.to(img.device) for ref_img_id in ref_img_ids], # 참조 이미지 patch별 id
-        "txt": txt.to(img.device),                                                # t5 임베딩 결과 
-        "txt_ids": txt_ids.to(img.device),                                        # 텍스트 토큰별 positional id
-        "vec": vec.to(img.device),                                                # clip 임베딩 결과
+        "img": img,                                                               # (B, 패치수, 패치채널)          # [16, 1008, 64]
+        "img_ids": img_ids.to(img.device),                                        # 이미지 patch별 positional id  # [16, 1008, 3]
+        "ref_img": tuple(ref_imgs_list),                                          # 참조 이미지 패치화 결과          # [16, 1008, 64] < inp['ref_img'][0]
+        "ref_img_ids": [ref_img_id.to(img.device) for ref_img_id in ref_img_ids], # 참조 이미지 patch별 id         # [16, 1008, 3]  < inp['ref_img_ids'][0]
+        "txt": txt.to(img.device),                                                # t5 임베딩 결과                 # [16,768]
+        "txt_ids": txt_ids.to(img.device),                                        # 텍스트 토큰별 positional id    #  [16, 768, 3]
+        "vec": vec.to(img.device),                                                # clip 임베딩 결과               # [16, 512, 4096]
     }
 
 
